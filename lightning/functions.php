551020aca530149368a77979e6457215be223f0a
travis-ci <kurudrive@gmail.com>
2019-01-22 17:56:46 +0000

    Update from travis 6269363f71fec6eb9d87be0cc53c006b397f1bf5

--- lightning/functions.php
+++ lightning/functions.php
@@ -117,9 +117,9 @@ function lightning_theme_setup() {
 
 add_action( 'wp_enqueue_scripts', 'lightning_addJs' );
 function lightning_addJs() {
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/library/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '3.3.4', true );
 	wp_enqueue_script( 'lightning-js', get_template_directory_uri() . '/assets/js/lightning.min.js', array( 'jquery' ), LIGHTNING_THEME_VERSION, true );
 }
-
 
 add_action( 'wp_enqueue_scripts', 'lightning_commentJs' );
 function lightning_commentJs() {
@@ -497,29 +497,4 @@ function lightning_deactivate_plugin( $plugin_path ) {
 		$active_plugins = array_values( $active_plugins );
 		update_option( 'active_plugins', $active_plugins );
 	}
-}
-
-add_filter( 'nav_menu_css_class', 'lightning_special_nav_class', 10, 2 );
-function lightning_special_nav_class( $classes, $item ) {
-
-	$defalut_post_type = array( 'post', 'page', 'attachment', 'revision' );
-	$post_type         = get_post_type();
-	$result            = in_array( $post_type, $defalut_post_type );
-
-	// カスタム投稿タイプを表示中かつ、カスタムアーカイブでなければ、current_page_parentクラスをこのメニューのみに追加。
-	if ( ! $result && is_singular( $post_type ) && ! is_post_type_archive() ) {
-
-		$index = array_search( 'current_page_parent', $classes );
-
-		//$classesが1から始まる連想配列で、$indexの数は+1されている。
-		//array_spliceはのoffsetは、0からの指定なので、-1を追加しているので注意。
-		array_splice( $classes, $index - 1, 1 );
-
-		if ( $item->object == 'custom' && ! in_array( 'current_page_parent', $classes ) ) {
-
-			$classes[] = 'current_page_parent';
-		}
-	}
-
-	return $classes;
 }
